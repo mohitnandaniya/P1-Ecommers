767d9abc53a9fea7d5ae3c73ff44a4d301901868
mohitnandaniya <mohitnandaniya1@gmail.com>
2024-06-03 21:28:08 +0530

    common commit

--- p1/src/App.jsx
+++ p1/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./comonents/Header";
+import Header from "./comonents/Navbar/Navbar";
 import "./styles/App.scss"
 
 function App() {
